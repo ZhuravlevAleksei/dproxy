87b4bd29aa6705403aa22ef92a52d705eaa79686
alex <zhuravlev-ms@outlook.com>
2022-10-19 23:11:37 +0400

    Client pool mode

--- src/client.c
+++ src/client.c
@@ -14,10 +14,14 @@
 #include "package.h"
 #include "storage.h"
 #include <threads.h>
+#include <sys/poll.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
 
 #define BUFLEN 512 // Max length of buffer
 #define SERVER "8.8.8.8"
 #define PORT 53 // The port on which to listen for incoming data
+#define RECVFROM_TIMEOUT_MS (60 * 1000)
 
 
 int init_client(void *t)
@@ -27,8 +31,12 @@ int init_client(void *t)
     int slen = sizeof(s_out_addr);
     int out_addr_len;
 
-    int s;
     int i;
+    int sd;
+    int on = 1;
+    struct pollfd fds[1];
+    int nfds = 1;
+    int timeout;
     int result;
     int recv_len;
     PkgContext pkg;
@@ -46,11 +54,27 @@ int init_client(void *t)
     char message[RESPONSE_PACKET_BUF_SIZE];
 
     // create UDP socket
-    s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    sd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-    if (s == -1)
+    if (sd == -1)
     {
         perror("socket");
+        thrd_exit(1);
+    }
+
+    result = setsockopt(sd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, (char *)&on, sizeof(on));
+
+    if (result == -1)
+    {
+        perror("setsockopt error");
+        thrd_exit(1);
+    }
+
+    result = ioctl(sd, FIONBIO, (char *)&on);
+
+    if (result == -1)
+    {
+        perror("ioctl error");
         thrd_exit(1);
     }
 
@@ -61,14 +85,26 @@ int init_client(void *t)
 
     if (inet_aton(SERVER, &s_out_addr.sin_addr) == 0)
     {
-        printf("inet_aton() failed\n");
-        close(s);
+        perror("inet_aton() failed\n");
+        close(sd);
         thrd_exit(1);
     }
 
     s_in_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    result = bind(s, (struct sockaddr *)&s_in_addr, sizeof(s_in_addr));
+    result = bind(sd, (struct sockaddr *)&s_in_addr, sizeof(s_in_addr));
+
+    if (result == -1)
+    {
+        perror("bind error");
+        thrd_exit(1);
+    }
+
+    memset(fds, 0, sizeof(fds));
+    fds[0].fd = sd;
+    fds[0].events = POLLIN;
+
+    timeout = RECVFROM_TIMEOUT_MS;
 
     init_srorage(&srg, &strg);
 
@@ -83,45 +119,59 @@ int init_client(void *t)
             continue;
         }
 
-        if (sendto(s, buf, recv_len, 0, (struct sockaddr *)&s_out_addr, slen) == -1)
+        if (sendto(sd, buf, recv_len, 0, (struct sockaddr *)&s_out_addr, slen) == -1)
         {
             perror("sendto()");
-            close(s);
+            close(sd);
             thrd_exit(1);
         }
 
         memset(buf, 0, BUFLEN);
 
-        recv_len = recvfrom(
-            s, buf, BUFLEN, 0, (struct sockaddr *)&s_out_addr, &out_addr_len);
+        result = poll(fds, nfds, timeout);
 
-        if (recv_len == -1)
+        if (result == -1)
         {
-            perror("recvfrom");
-            close(s);
+            perror("poll error");
+            close(sd);
             thrd_exit(1);
         }
-
-        if (!json_get_addr(message, &addr, &port, &transaction))
+        else if (result > 0)
         {
+            recv_len = recvfrom(
+                sd, buf, BUFLEN, 0, (struct sockaddr *)&s_out_addr, &out_addr_len);
+
+            if (recv_len == -1)
+            {
+                perror("recvfrom");
+            }
+
+            if (!json_get_addr(message, &addr, &port, &transaction))
+            {
+                continue;
+            }
+
+            for(i = 0; i < recv_len; i++)
+            {
+                sprintf((message + (i * 2)), "%02x", buf[i]);
+            }
+
+            message[(recv_len * 2)] = 0;
+
+            memset(buf, 0, BUFLEN);
+
+            response_to_json(buf, addr, port, transaction, message);
+
+            write_buffer(srg, SERVER_COMM_BUFFER_IN, buf);
+        }
+        else
+        {
+            printf("Packet was lost. Timeout:%u\n", RECVFROM_TIMEOUT_MS);
             continue;
         }
-
-        for(i = 0; i < recv_len; i++)
-        {
-            sprintf((message + (i * 2)), "%02x", buf[i]);
-        }
-
-        message[(recv_len * 2)] = 0;
-
-        memset(buf, 0, BUFLEN);
-
-        response_to_json(buf, addr, port, transaction, message);
-
-        write_buffer(srg, SERVER_COMM_BUFFER_IN, buf);
     }
 
-    close(s);
+    close(sd);
 
     thrd_exit(0);
 }
